e6e5845c26ba086994328146daeef7a5c7b121e2
Kaan <kk@flow.city>
2023-07-18 14:56:03 +0100

    user table creation and data insertion

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -8,26 +8,21 @@ class Data(BaseModel):
     some_field: str  
 
 @app.post('/')
-async def read_root(request: Request):
+async def post_root(request: Request):
     data = await request.json()
     print("hi this is a log")
     print(data)
     return {'Hello': 'Docker with FastAPI!'}
 
 @app.get('/')
-async def read_root(request: Request):
-    data = await request.json()
-    print("hi this is a log")
-    print(data)
-    return {'Hello': 'Docker with FastAPI!'}
+async def read_root():
+    return {'Hello': 'Docker with FastAPI! You just Read the Root Get Request'}
 
 @app.post('/evt')
-def read_root(data: Data):
+def post_event(data: Data):
     print(data.some_field)
     return {'Hello': 'Docker with FastAPI!', 'Your Data': data.some_field}
 
 @app.get('/evt')
-def read_event(data):
-    print("hi this is a log")
-    print(data)
+def read_event():
     return {'Hello': 'welcome to the event endpoint'}
