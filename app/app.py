1dafd05f4a531e25d0cb386e7b183b70c89b4640
unknown <bionixs88@gmail.com>
2023-07-18 16:47:08 +0100

    add redis service to docker and write user data to redis from app

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+import redis
 
 app = FastAPI()
 
@@ -7,6 +8,22 @@ async def read_root(request: Request):
     headers = request.headers
     for key, value in headers.items():
         print(f"{key}:{value}")
+    r = redis.Redis(host='cache', port=6379, decode_responses=True)
+    r.set("user",headers.get('user') )
+    # True
+    print(r.get("user"))
+    r.set("session", headers.get('session'))
+    print(r.get("session"))
+    print(r.hset('user-session:123', mapping={
+        'name': 'John',
+        "surname": 'Smith',
+        "company": 'Redis',
+        "age": 29
+    }))
+    # True
+
+    r.hgetall('user-session:123')
+    # {'surname': 'Smith', 'name': 'John', 'company': 'Redis', 'age': '29'}
     return 'https://tiny-texts-jk9apq.s3.us-east-1.amazonaws.com/0007933c-e702-4ccf-872c-d5235d6ee51a.txt'
 
 @app.post('/evt')
@@ -15,3 +32,5 @@ def read_event(request: Request):
     for key, value in headers.items():
         print(f"{key}:{value}")
     return 'Hello from event route'
+
+
