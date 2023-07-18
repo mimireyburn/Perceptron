2595ba8a2a291b431cfda261c9ac608d488eb535
Kaan <kk@flow.city>
2023-07-18 17:04:58 +0100

    commit afyer merge

--- app/app.py
+++ app/app.py
@@ -1,35 +1,65 @@
-from fastapi import FastAPI, Request
-import redis
 from fastapi import FastAPI, Request, HTTPException
+import redis
 import psycopg2
 import json
 
 app = FastAPI()
 
+connection_info = {
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "Perceptron",
+    "host": "localhost",
+    "port": 5433  # or any port you have configured
+}
+
 @app.get('/')
 async def read_root(request: Request):
     headers = request.headers
-    for key, value in headers.items():
-        print(f"{key}:{value}")
+    userid = headers.get('user')
+    session = headers.get('session')
+    
+    if userid is None or session is None:
+        raise HTTPException(status_code=400, detail="Missing user or session in headers")
+
     r = redis.Redis(host='cache', port=6379, decode_responses=True)
-    r.set("user",headers.get('user') )
-    r.set("session", headers.get('session'))
-    r.hset('user-session:123', mapping={
+    r.set(f"user:{session}", userid)
+    r.set(f"session:{session}", session)
+    r.hset(f'user-session:{session}', mapping={
         'name': 'John',
         "surname": 'Smith',
         "company": 'Redis',
         "age": 29
     })
 
-    r.hgetall('user-session:123')
-    # {'surname': 'Smith', 'name': 'John', 'company': 'Redis', 'age': '29'}
-    return '0007933c-e702-4ccf-872c-d5235d6ee51a'
+    item = item_returner(connection_info, userid)
+
+    user_data = r.hgetall(f'user-session:{session}')
+
+    return {"item": item, "user_data": user_data}
 
 @app.post('/evt')
 def read_event(request: Request):
     headers = request.headers
     for key, value in headers.items():
         print(f"{key}:{value}")
-        
     
     return 'Hello from event route'
+
+def item_returner(connection_info, userid):
+    conn = psycopg2.connect(**connection_info)
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        SELECT item_key FROM items WHERE user_id = %s
+        """,
+        (userid,)  # Notice the comma, making it a tuple
+    )
+    item_keys = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return item_keys
+
