747bb01b2981638299ee1628c491cdc537cd30ad
amberrignell <36554605+amberrignell@users.noreply.github.com>
2023-07-18 12:58:28 +0100

    return item

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ async def read_root(request: Request):
     headers = request.headers
     for key, value in headers.items():
         print(f"{key}:{value}")
-    return 'Hello from get route'
+    return 'https://tiny-texts-jk9apq.s3.us-east-1.amazonaws.com/0007933c-e702-4ccf-872c-d5235d6ee51a.txt'
 
 @app.post('/evt')
 def read_event(request: Request):
