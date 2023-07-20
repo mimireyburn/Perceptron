5fa90cc2e07bc5610d56473c1362a189011073a0
Mimi Reyburn <mimireyburn@gmail.com>
2023-07-20 15:43:44 +0100

    Set redis variables correctly

--- app/app.py
+++ app/app.py
@@ -20,9 +20,9 @@ async def read_root(request: Request):
     # Get the current datetime
     current_datetime = datetime.now().isoformat()
 
-    r.hset(f"{userid}", "user_id", userid)
-    r.hset(f"user:{userid}", "session_id", session)
-    r.hset(f"user:{userid}", "timestamp", current_datetime)
+    r.set(f"user:{userid}", userid)
+    r.set(f"session:{session}", session)
+    r.set(f"datetime:{current_datetime}", current_datetime)
     
     user_recommendations = r.zrange('recommendations', 0, -1, withscores=True)
 
@@ -63,37 +63,3 @@ def read_event(request: Request):
         print(f"{key}:{value}")
     
     return 'Hello from event route'
-
-
-# connection_info = {
-    # "database": "postgres",
-    # "user": "postgres",
-    # "password": "Perceptron",
-    # "host": "localhost",
-    # "port": 5433  # or any port you have configured
-    # }
-    
-    # if userid is None or session is None:
-    #     raise HTTPException(status_code=400, detail="Missing user or session in headers")
-
-
-    # def item_returner(connection_info, userid):
-    #     conn = pg8000.connect(**connection_info)
-    #     cur = conn.cursor()
-
-    #     cur.execute(
-    #         """
-    #         SELECT item_key FROM items WHERE user_id = %s
-    #         """,
-    #         (userid,)  # Notice the comma, making it a tuple
-    #     )
-    #     item_keys = cur.fetchall()
-
-    #     cur.close()
-    #     conn.close()
-
-    #     return item_keys
-
-    # item = item_returner(connection_info, userid)
-
-    # user_data = r.hgetall(f'user-session:{session}')
